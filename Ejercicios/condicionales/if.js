0ddcd813176a34e7cb9c76d816d4b5b8c0ddaff8
brice180 <brice180@gmail.com>
2020-01-08 12:07:25 -0500

    Martes

---
+++ Ejercicios/condicionales/if.js
@@ -0,0 +1,4 @@
+var cantidad = 15;
+if (cantidad > 10) {
+    console.log("Se debe aplicar el descuento");
+}
---
+++ Ejercicios/condicionales/if.ts
@@ -0,0 +1,5 @@
+var cantidad:number=15;
+
+if(cantidad>10){
+    console.log("Se debe aplicar el descuento");
+}
